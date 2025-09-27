230e5ad5565c661c6209c2f05b08a55a77ed8000
maticpl <76453898+maticpl@users.noreply.github.com>
2025-09-27 21:11:54 +0200

    firebase-messaging-sw.js

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,4 @@
+// Importuj i skonfiguruj Firebase w Service Workerze
+// Nadaj temu plikowi nazwę firebase-messaging-sw.js i umieść go w głównym katalogu
+importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js");
