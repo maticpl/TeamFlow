41f8919788994040b87d650633108b94277822bc
maticpl <76453898+maticpl@users.noreply.github.com>
2025-09-27 21:12:31 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -2,3 +2,18 @@
 // Nadaj temu plikowi nazwę firebase-messaging-sw.js i umieść go w głównym katalogu
 importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-app-compat.js");
 importScripts("https://www.gstatic.com/firebasejs/11.6.1/firebase-messaging-compat.js");
+
+// Twoja konfiguracja Firebase
+// WAŻNE: Musi być taka sama jak w Twojej aplikacji
+const firebaseConfig = {
+    apiKey: "__API_KEY__",
+    authDomain: "__AUTH_DOMAIN__",
+    projectId: "__PROJECT_ID__",
+    storageBucket: "__STORAGE_BUCKET__",
+    messagingSenderId: "__MESSAGING_SENDER_ID__",
+    appId: "__APP_ID__"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
